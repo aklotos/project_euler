d24cedb7d71fa22712c7920385d3b30281d3703a
Anton Klimenko <ak.igorevich@gmail.com>
2016-11-26 04:17:52 +0300

    Solved problem 9 and 10

---
+++ erlang/src/problem_10.erl
@@ -0,0 +1,22 @@
+%%%-------------------------------------------------------------------
+%%% Problem 10
+%%% The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+%%% Find the sum of all the primes below two million.
+%%%-------------------------------------------------------------------
+-module(problem_10).
+-author("akli").
+
+%% API
+-export([result/0]).
+
+result() ->
+  Numbers = [2 | [N || N <- lists:seq(2,2000000), N rem 2 =/= 0] ],
+  Primes = sieve(Numbers, {3, trunc(math:sqrt(2000000)) + 1}),
+  lists:sum(Primes).
+
+sieve(Numbers, {Next, Max}) when Next > Max ->
+  Numbers;
+sieve(Numbers, {Next, Max}) ->
+  Sieved = lists:filter(fun(X) -> X == Next orelse X rem Next =/= 0 end, Numbers),
+  [H|_] = lists:dropwhile(fun(E) -> E =< Next end, Sieved),
+  sieve(Sieved, {H, Max}).
