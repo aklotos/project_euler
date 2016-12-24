bf54daed68b73982456c5ce171b083f7758f4373
Anton Klimenko <ak.igorevich@gmail.com>
2016-12-25 02:33:13 +0300

    Solved problem 20

---
+++ nodejs/lib/problem_20.js
@@ -0,0 +1,28 @@
+/**
+ Problem 20
+ n! means n × (n − 1) × ... × 3 × 2 × 1
+
+ For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+ and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+ Find the sum of the digits in the number 100!
+ */
+
+'use strict';
+
+const bigint = require('big-integer');
+
+function problem20() {
+    let prod = bigint(1);
+    for (let i = 1; i < 101; i++) {
+        prod = prod.multiply(i);
+    }
+
+    let result = prod.toString().split('').reduce((result, v) => {
+        return result + parseInt(v)
+    }, 0);
+
+    console.log(result);
+}
+
+problem20();
